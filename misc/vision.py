bf4e9ec45d0d74272aaa5abadd0cfe83dbb3727b
Joel Fuchs <joel.s.fuchs@gmail.com>
2018-09-05 15:06:57 +0200

    insterted blob detection for blue and yellow

--- misc/vision.py
+++ misc/vision.py
@@ -21,14 +21,14 @@ def detect_blob(agent):
 
 def detect_red_blob(image):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([5, 50, 50])
-    upper_red = np.array([15, 255, 255])
-    mask0 = cv2.inRange(img, lower_red, upper_red)
+    lower_boundary = np.array([5, 50, 50])
+    upper_boundary = np.array([15, 255, 255])
+    mask0 = cv2.inRange(img, lower_boundary, upper_boundary)
     #cv2.imshow("Mask0", mask0)
 
-    lower_red = np.array([170, 50, 50])
-    upper_red = np.array([180, 255, 255])
-    mask1 = cv2.inRange(img, lower_red, upper_red)
+    lower_boundary = np.array([170, 50, 50])
+    upper_boundary = np.array([180, 255, 255])
+    mask1 = cv2.inRange(img, lower_boundary, upper_boundary)
     #cv2.imshow("Mask1", mask1)
 
     mask = mask0 + mask1
@@ -73,13 +73,98 @@ def detect_red_blob(image):
 
     return center
 
+
 def detect_yellow_blob(image):
-    center = -1
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_boundary = np.array([20, 100, 100])
+    upper_boundary = np.array([30, 255, 255])
+    mask = cv2.inRange(img, lower_boundary, upper_boundary)
+    #cv2.imshow("Mask0", mask0)
+
+
+    _, cnts, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # area1 and area2 are the range of contour area, change accordingly
+    area1 = 100
+    area2 = 2000000
+    totalDots = []
+    max_area = -100
+    cnt_max = 0
+
+    # Count the total number of contours
+    for cnt in cnts:
+        if area1 < cv2.contourArea(cnt) < area2:
+            print(cv2.contourArea(cnt))
+            if cv2.contourArea(cnt) > max_area:
+                max_area = cv2.contourArea(cnt)
+                cnt_max = cnt
+            totalDots.append(cnt)
+
+    # getting position of biggest blob
+    if len(totalDots) > 0:
+        (x, y), radius = cv2.minEnclosingCircle(cnt_max)
+        center = (int(x), int(y))
+        radius = int(radius)
+        cv2.circle(img, center, radius, (0, 255, 0), 2)
+        print("center:")
+        print(center)
+    else:
+        print("no dots found!")
+        return -1
+
+    text = "Total number of dots are: {}".format(len(totalDots))
+    cv2.putText(mask, text, (50, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (200, 255, 255), 2)
+    #cv2.imshow("Mask", mask)
+    #cv2.waitKey(10000)
+    #cv2.destroyAllWindows()
+    print("Total number of dots are:{}".format(len(totalDots)))
 
     return center
 
 def detect_blue_blob(image):
-    center = -1
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_boundary = np.array([100,150,0])
+    upper_boundary = np.array([140,255,255])
+    mask = cv2.inRange(img, lower_boundary, upper_boundary)
+    #cv2.imshow("Mask0", mask0)
+
+
+    _, cnts, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # area1 and area2 are the range of contour area, change accordingly
+    area1 = 100
+    area2 = 2000000
+    totalDots = []
+    max_area = -100
+    cnt_max = 0
+
+    # Count the total number of contours
+    for cnt in cnts:
+        if area1 < cv2.contourArea(cnt) < area2:
+            print(cv2.contourArea(cnt))
+            if cv2.contourArea(cnt) > max_area:
+                max_area = cv2.contourArea(cnt)
+                cnt_max = cnt
+            totalDots.append(cnt)
+
+    # getting position of biggest blob
+    if len(totalDots) > 0:
+        (x, y), radius = cv2.minEnclosingCircle(cnt_max)
+        center = (int(x), int(y))
+        radius = int(radius)
+        cv2.circle(img, center, radius, (0, 255, 0), 2)
+        print("center:")
+        print(center)
+    else:
+        print("no dots found!")
+        return -1
+
+    text = "Total number of dots are: {}".format(len(totalDots))
+    cv2.putText(mask, text, (50, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (200, 255, 255), 2)
+    #cv2.imshow("Mask", mask)
+    #cv2.waitKey(10000)
+    #cv2.destroyAllWindows()
+    print("Total number of dots are:{}".format(len(totalDots)))
 
     return center
 
